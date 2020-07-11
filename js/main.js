81df896d8f35eea479c0ba658eb64184ac3f802c
alakbar289 <60427636+alakbar289@users.noreply.github.com>
2020-07-11 21:09:34 +0400

    js elave

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function hover(){
+    document.getElementById("h-menu-id").classList.remove("blok");
+    document.getElementById("h-menu-id").classList.add("hidde-menu");
+    }
+function out(){
+    document.getElementById("h-menu-id").classList.remove("hidde-menu");
+    document.getElementById("h-menu-id").classList.add("blok");
+}
+
